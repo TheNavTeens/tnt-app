df5c39aefbf3edb15b3cba6caa538d0860002c14
julwrites <julian.teh@outlook.com>
2022-03-29 23:02:52 +0800

    Adding e1 link redirect

---
+++ src/pages/e1/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class E1 extends React.Component {
+    componentDidMount () {
+        window.location.href = "https://docs.google.com/spreadsheets/d/1GlDG2Bt4RIieK5Y5YBNbvGkvNwM6Ve8PSdb3DAJbuZQ/edit#gid=0";
+    }
+
+    render () {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default E1;
