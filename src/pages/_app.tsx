d2973650d31269daa6de1362548b9dbdeb9398d0
julwrites <mail@tehj.io>
2022-10-06 10:33:05 +0800

    Adding Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <title>TNT Online</title>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </ChakraProvider>
   )
