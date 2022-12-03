0dd3ef2e0478c9996173bd96d1c7c3c7c11be26b
julwrites <mail@tehj.io>
2022-12-03 16:34:37 +0800

    Switching to a simpler re-direct mechanism

--- src/pages/cal/index.tsx
+++ src/pages/cal/index.tsx
@@ -1,46 +1,15 @@
-import {
-  VStack
-} from '@chakra-ui/react'
+import React from 'react';
 
-import { Container } from '../../components/Container'
-import { Banner } from '../../components/Banner'
+class TNTCalendar extends React.Component {
+    componentDidMount () {
+        window.location.href = "https://calendar.google.com/calendar/embed?src=l9kdcd1fccejrbtt8cms1kpkk4%40group.calendar.google.com&ctz=Asia%2FSingapore";
+    }
 
-// import { useEffect, useState } from 'react'
-
-const TNTCalendar = () => {
-  // const [width, setWidth] = useState(0)
-  // const [height, setHeight] = useState(0)
-  // useEffect(() => {
-  //   const maxHeight = 2200
-  //   const maxWidth = 640
-  //   const refreshDimensions = () => {
-  //     const width = Math.min(maxWidth, window.innerWidth - 40)
-  //     setWidth(width)
-  //     setHeight(maxHeight * 0.6 * (maxWidth / width))
-  //   }
-
-  //   window.addEventListener("resize", () => {
-  //     refreshDimensions()
-  //   })
-  //   refreshDimensions()
-  // }, [])
-
-  return (
-    <div>
-      <Container height="100vh">
-        <Banner />
-        <VStack overflow="scroll">
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Asia%2FSingapore&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&src=bDlrZGNkMWZjY2VqcmJ0dDhjbXMxa3BrazRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%237986CB"
-          width="800"
-          height="600"
-          frameBorder="0"
-          marginHeight={0}
-          marginWidth={0}>
-          Loading...</iframe>
-        </VStack>
-      </Container>
-    </div>
-  )
+    render () {
+        return (
+            <div></div>
+        )
+    }
 }
 
-export default TNTCalendar
+export default TNTCalendar;
