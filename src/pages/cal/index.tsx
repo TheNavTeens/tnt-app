e59e3a02739f070267f56d239cb5a08d4c87a114
julwrites <mail@tehj.io>
2022-12-03 16:14:39 +0800

    Adding a calendar page

---
+++ src/pages/cal/index.tsx
@@ -0,0 +1,48 @@
+import {
+  VStack
+} from '@chakra-ui/react'
+
+import { Container } from '../../components/Container'
+import { Banner } from '../../components/Banner'
+
+import { useEffect, useState } from 'react'
+
+const BookTJ = () => {
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+  useEffect(() => {
+    const maxHeight = 2200
+    const maxWidth = 640
+    const refreshDimensions = () => {
+      const width = Math.min(maxWidth, window.innerWidth - 40)
+      setWidth(width)
+      setHeight(maxHeight * 0.6 * (maxWidth / width))
+    }
+
+    window.addEventListener("resize", () => {
+      refreshDimensions()
+    })
+    refreshDimensions()
+  }, [])
+
+  return (
+    <div>
+      <Container height="100vh">
+        <Banner />
+        <VStack overflow="scroll">
+        <iframe 
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Asia%2FSingapore&showTitle=0&showPrint=0&showTabs=0&showCalendars=0&src=bDlrZGNkMWZjY2VqcmJ0dDhjbXMxa3BrazRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%237986CB" 
+        width={width}
+        height={height}
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+        scrolling="no">
+          Loading...</iframe>
+        </VStack>
+      </Container>
+    </div>
+  )
+}
+
+export default BookTJ
