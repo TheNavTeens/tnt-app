4b546ea71c22e4c80017173266c166e0ad297765
julwrites <mail@tehj.io>
2023-03-01 07:20:38 +0800

    Adding CoP Feedback redirect; TODO: add page for resources

---
+++ src/pages/cop-feedback/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CoPFeedback extends React.Component {
+    componentDidMount () {
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSd1Z5n3LjQIdw5hMavBcqXxA81iez5vWE3ME5fQsgF-tz_GPQ/viewform?usp=sf_link";
+    }
+
+    render () {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default CoPFeedback;
