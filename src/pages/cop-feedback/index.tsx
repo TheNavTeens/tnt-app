64e3a2790fd37a494a6ba64c027d57b6bb0d13c5
julwrites <mail@tehj.io>
2023-03-04 23:01:53 +0800

    Setting up a toggle for feedback form

--- src/pages/cop-feedback/index.tsx
+++ src/pages/cop-feedback/index.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 class CoPFeedback extends React.Component {
     componentDidMount () {
+        let session = "discussion";
+        if (session == "teaching") {
+        // Teaching Session feedback
         window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSd1Z5n3LjQIdw5hMavBcqXxA81iez5vWE3ME5fQsgF-tz_GPQ/viewform?usp=sf_link";
+        } else if (session == "discussion") {
+        // Discussion Session Feedback
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSenrheUf5MggB-AY5hNHWiIrrb3Dj6hfELftkVt3yy7rGMvSA/viewform?usp=sf_link";
+        }
     }
 
     render () {
