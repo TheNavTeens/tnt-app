7f65f22632bc48c0655d209d84b0039bfedb0325
Julian Teh <mail@tehj.io>
2023-04-03 22:55:11 +0800

    Update index.tsx

--- src/pages/cop-feedback/index.tsx
+++ src/pages/cop-feedback/index.tsx
@@ -6,10 +6,10 @@ class CoPFeedback extends React.Component {
 
         if (session == "teaching") {
         // Teaching Session feedback
-          window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSd1Z5n3LjQIdw5hMavBcqXxA81iez5vWE3ME5fQsgF-tz_GPQ/viewform?usp=sf_link";
+            window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSd1Z5n3LjQIdw5hMavBcqXxA81iez5vWE3ME5fQsgF-tz_GPQ/viewform?usp=sf_link";
         } else if (session == "discussion") {
         // Discussion Session Feedback
-          window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSenrheUf5MggB-AY5hNHWiIrrb3Dj6hfELftkVt3yy7rGMvSA/viewform?usp=sf_link";
+            window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSenrheUf5MggB-AY5hNHWiIrrb3Dj6hfELftkVt3yy7rGMvSA/viewform?usp=sf_link";
         }
     }
 
