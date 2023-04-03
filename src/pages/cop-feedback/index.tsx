ebbd33e6c67a81559499220e3915e1046113b062
Julian Teh <mail@tehj.io>
2023-04-03 22:42:10 +0800

    Update index.tsx

--- src/pages/cop-feedback/index.tsx
+++ src/pages/cop-feedback/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class CoPFeedback extends React.Component {
     componentDidMount () {
-        let session = "discussion";
+        let session = "teaching";
         if (session == "teaching") {
         // Teaching Session feedback
         window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSd1Z5n3LjQIdw5hMavBcqXxA81iez5vWE3ME5fQsgF-tz_GPQ/viewform?usp=sf_link";
