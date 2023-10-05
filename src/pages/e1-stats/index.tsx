e923a185b2dbec80f2b37bee6360e54a134a0b29
julian <julian.teh@screeningeagle.com>
2023-10-05 15:28:51 +0200

    Adding pages for e1, e2 and e3 statistics

---
+++ src/pages/e1-stats/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class E1Statistics extends React.Component {
+    componentDidMount () {
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfrbd2PKVC98i_WoT1WKnY_2TFsw-TADiE3dDXgrbbjdnQWqw/viewform?usp=sf_link";
+    }
+
+    render () {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default E1Statistics;
---
+++ src/pages/e2-stats/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class E2Statistics extends React.Component {
+    componentDidMount () {
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfDvfTuHh3mtVA-I75HyFC7MwF3QJ1iLc2eATuJCaWuwY7vQg/viewform?usp=sf_link";
+    }
+
+    render () {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default E2Statistics;
