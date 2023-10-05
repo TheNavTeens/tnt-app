9f3d97b298a700000744fec06d6728090057c672
julian <julian.teh@screeningeagle.com>
2023-10-05 15:54:35 +0200

    Updating spreadsheet link

---
+++ src/pages/e1-e3-stats/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class E1E2E3Statistics extends React.Component {
+    componentDidMount () {
+        window.location.href = "https://docs.google.com/spreadsheets/d/1t7uKM1FsgonP77tT3R2MUy1wykFpuPAPEgD4tBXyxKw/edit?usp=sharing";
+    }
+
+    render () {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default E1E2E3Statistics;
