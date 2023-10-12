505fca2384ab14a9bd3c72df1ea01705a536eca0
julian <julian.teh@screeningeagle.com>
2023-10-12 12:33:07 +0200

    Removing parameters that aren't in tj booking form

--- src/pages/e1-e3-dashboard/index.tsx
+++ src/pages/e1-e3-dashboard/index.tsx
@@ -44,28 +44,28 @@ const E1E3Dashboard = () => {
             <TabPanel>
               <H2>E1</H2>
               <VStack overflow="scroll">
-                <iframe width="800" height="500" seamless frameBorder="0" scrolling="yes" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSLOWTa8Qo8WWSZ6PZLOTaKOVGia-vdPmqOa3dY2OUkSNvV-G1yN_x7hruLqw0ytj4giXFJAHUtjA5Z/pubchart?oid=803854579&amp;format=interactive"></iframe>
+                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSLOWTa8Qo8WWSZ6PZLOTaKOVGia-vdPmqOa3dY2OUkSNvV-G1yN_x7hruLqw0ytj4giXFJAHUtjA5Z/pubchart?oid=803854579&amp;format=interactive"></iframe>
               </VStack>
               <VStack overflow="scroll">
-                <iframe width="800" height="500" seamless frameBorder="0" scrolling="yes" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSLOWTa8Qo8WWSZ6PZLOTaKOVGia-vdPmqOa3dY2OUkSNvV-G1yN_x7hruLqw0ytj4giXFJAHUtjA5Z/pubchart?oid=37917538&amp;format=interactive"></iframe>
+                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSLOWTa8Qo8WWSZ6PZLOTaKOVGia-vdPmqOa3dY2OUkSNvV-G1yN_x7hruLqw0ytj4giXFJAHUtjA5Z/pubchart?oid=37917538&amp;format=interactive"></iframe>
               </VStack>
             </TabPanel>
             <TabPanel>
               <H2>E2</H2>
               <VStack overflow="scroll">
-                <iframe width="800" height="500" seamless frameBorder="0" scrolling="yes" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFDo1u1WNeF_sQu8VHKPt9sE-763WbzfYzgQYsuIPxn7Wpa9WmtDFrmKdrfKF-P-FK8q-tM2427jb/pubchart?oid=1534404840&amp;format=interactive"></iframe>
+                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFDo1u1WNeF_sQu8VHKPt9sE-763WbzfYzgQYsuIPxn7Wpa9WmtDFrmKdrfKF-P-FK8q-tM2427jb/pubchart?oid=1534404840&amp;format=interactive"></iframe>
               </VStack>
               <VStack overflow="scroll">
-                <iframe width="800" height="500" seamless frameBorder="0" scrolling="yes" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFDo1u1WNeF_sQu8VHKPt9sE-763WbzfYzgQYsuIPxn7Wpa9WmtDFrmKdrfKF-P-FK8q-tM2427jb/pubchart?oid=396302033&amp;format=interactive"></iframe>
+                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFDo1u1WNeF_sQu8VHKPt9sE-763WbzfYzgQYsuIPxn7Wpa9WmtDFrmKdrfKF-P-FK8q-tM2427jb/pubchart?oid=396302033&amp;format=interactive"></iframe>
               </VStack>
             </TabPanel>
             <TabPanel>
               <H2>E3</H2>
               <VStack overflow="scroll">
-                <iframe width="800" height="500" seamless frameBorder="0" scrolling="yes" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIEnxEqwCNwNj1QzMDOrhwJ4UBaWRvT5_9HImQ585-AFu5tq5rg-s_UHqeeMmPXdqa-3UyS-LZM8BB/pubchart?oid=991987233&amp;format=interactive"></iframe>
+                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIEnxEqwCNwNj1QzMDOrhwJ4UBaWRvT5_9HImQ585-AFu5tq5rg-s_UHqeeMmPXdqa-3UyS-LZM8BB/pubchart?oid=991987233&amp;format=interactive"></iframe>
               </VStack>
               <VStack overflow="scroll">
-                <iframe width="800" height="500" seamless frameBorder="0" scrolling="yes" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIEnxEqwCNwNj1QzMDOrhwJ4UBaWRvT5_9HImQ585-AFu5tq5rg-s_UHqeeMmPXdqa-3UyS-LZM8BB/pubchart?oid=458433379&amp;format=interactive"></iframe>
+                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIEnxEqwCNwNj1QzMDOrhwJ4UBaWRvT5_9HImQ585-AFu5tq5rg-s_UHqeeMmPXdqa-3UyS-LZM8BB/pubchart?oid=458433379&amp;format=interactive"></iframe>
               </VStack>
             </TabPanel>
           </TabPanels>
