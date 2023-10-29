e8c7a00190fda515c058ed0daca2cadbc0e2d681
Julian Teh <mail@tehj.io>
2023-10-29 10:14:49 +0800

    Consolidating into Evangelism and Discipling dashboards

--- src/pages/e1-e3-dashboard/index.tsx
+++ src/pages/e1-e3-dashboard/index.tsx
@@ -25,29 +25,19 @@ const E1E3Dashboard = () => {
           <TabPanels>
             <TabPanel>
               <VStack overflow="scroll">
-                <Heading>E1</Heading>
+                <Heading>Evangelism</Heading>
                 <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSLOWTa8Qo8WWSZ6PZLOTaKOVGia-vdPmqOa3dY2OUkSNvV-G1yN_x7hruLqw0ytj4giXFJAHUtjA5Z/pubchart?oid=803854579&amp;format=interactive"></iframe>
                 <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSLOWTa8Qo8WWSZ6PZLOTaKOVGia-vdPmqOa3dY2OUkSNvV-G1yN_x7hruLqw0ytj4giXFJAHUtjA5Z/pubchart?oid=37917538&amp;format=interactive"></iframe>
               </VStack>
             </TabPanel>
             <TabPanel>
               <VStack overflow="scroll">
-                <Heading>E2</Heading>
+                <Heading>Discipling</Heading>
                 <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFDo1u1WNeF_sQu8VHKPt9sE-763WbzfYzgQYsuIPxn7Wpa9WmtDFrmKdrfKF-P-FK8q-tM2427jb/pubchart?oid=1534404840&amp;format=interactive"></iframe>
                 <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFDo1u1WNeF_sQu8VHKPt9sE-763WbzfYzgQYsuIPxn7Wpa9WmtDFrmKdrfKF-P-FK8q-tM2427jb/pubchart?oid=1616511846&amp;format=interactive"></iframe>
                 <Heading size="lg">Stat Breakdown</Heading>
                 <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFDo1u1WNeF_sQu8VHKPt9sE-763WbzfYzgQYsuIPxn7Wpa9WmtDFrmKdrfKF-P-FK8q-tM2427jb/pubchart?oid=1720305951&amp;format=interactive"></iframe>
                 <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFDo1u1WNeF_sQu8VHKPt9sE-763WbzfYzgQYsuIPxn7Wpa9WmtDFrmKdrfKF-P-FK8q-tM2427jb/pubchart?oid=363597148&amp;format=interactive"></iframe>
-              </VStack>
-            </TabPanel>
-            <TabPanel>
-              <VStack overflow="scroll">
-                <Heading>E3</Heading>
-                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIEnxEqwCNwNj1QzMDOrhwJ4UBaWRvT5_9HImQ585-AFu5tq5rg-s_UHqeeMmPXdqa-3UyS-LZM8BB/pubchart?oid=991987233&amp;format=interactive"></iframe>
-                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIEnxEqwCNwNj1QzMDOrhwJ4UBaWRvT5_9HImQ585-AFu5tq5rg-s_UHqeeMmPXdqa-3UyS-LZM8BB/pubchart?oid=537967256&amp;format=interactive"></iframe>
-                <Heading size="lg">Stat Breakdown</Heading>
-                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIEnxEqwCNwNj1QzMDOrhwJ4UBaWRvT5_9HImQ585-AFu5tq5rg-s_UHqeeMmPXdqa-3UyS-LZM8BB/pubchart?oid=458433379&amp;format=interactive"></iframe>
-                <iframe width="800" height="500" frameBorder="0" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQIEnxEqwCNwNj1QzMDOrhwJ4UBaWRvT5_9HImQ585-AFu5tq5rg-s_UHqeeMmPXdqa-3UyS-LZM8BB/pubchart?oid=207964844&amp;format=interactive"></iframe>
               </VStack>
             </TabPanel>
           </TabPanels>
