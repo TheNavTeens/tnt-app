2f9cc9562d7132287fe34108e602fdd2f5a0d41d
Julian Teh <julian.teh@outlook.com>
2024-08-29 20:05:28 +0800

    Adding CoP recordings page

---
+++ src/pages/cop-recordings/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CoPRecordings extends React.Component {
+    componentDidMount () {
+        window.location.href = "https://www.youtube.com/playlist?list=PLvDmTpB1lr7hcn8akc92TR86d4YXwOhYl";
+    }
+
+    render () {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default CoPRecordings;
