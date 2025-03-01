d0e7607bb0e7ebc7ee410cb4709248474118a6fe
julwrites <mail@tehj.io>
2025-03-01 14:13:34 +0800

    Fixing link

--- src/pages/tznav-cop-feedback/index.tsx
+++ src/pages/tznav-cop-feedback/index.tsx
@@ -6,7 +6,7 @@ class TzNavCoPFeedback extends React.Component {
 
         if (session == "disciple-making") {
         // Discussion Session Feedback
-            window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSdNuzoHu3y2LL3Rl9qzlrz7QGbAMCUgHeIeOvOA-ZiL9uJIDQ/viewform?usp=sf_link";
+            window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScd0eAMO4UUfRFZ3k2alPrScYPbts--fsho960BSZFpdiaUqA/viewform?usp=header";
         } else if (session == "leadership") {
         // Retreat Session Feedback
             window.location.href = "";
