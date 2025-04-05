3e6ae22011645e3db6b019761911d4ac4bfe9e4d
julwrites <mail@tehj.io>
2025-04-05 20:38:35 +0800

    Feedback form for Leadership session

--- src/pages/tznav-cop-feedback/index.tsx
+++ src/pages/tznav-cop-feedback/index.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
 class TzNavCoPFeedback extends React.Component {
-    componentDidMount () {
-        let session = "disciple-making";
+  componentDidMount() {
+    let session = "leadership";
 
-        if (session == "disciple-making") {
-        // Discussion Session Feedback
-            window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScd0eAMO4UUfRFZ3k2alPrScYPbts--fsho960BSZFpdiaUqA/viewform?usp=header";
-        } else if (session == "leadership") {
-        // Retreat Session Feedback
-            window.location.href = "";
-        } else if (session == "vision") {
-        // Retreat Session Feedback
-            window.location.href = "";
-        } else if (session == "intention") {
-        // Retreat Session Feedback
-            window.location.href = "";
-        }
+    if (session == "disciple-making") {
+      // Discussion Session Feedback
+      window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScd0eAMO4UUfRFZ3k2alPrScYPbts--fsho960BSZFpdiaUqA/viewform?usp=header";
+    } else if (session == "leadership") {
+      // Retreat Session Feedback
+      window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfpWYV6Ar7RAG-r3ySULHdVtmbuYpp40TaqUgrKZUn_o0UEWg/viewform?usp=header";
+    } else if (session == "vision") {
+      // Retreat Session Feedback
+      window.location.href = "";
+    } else if (session == "intention") {
+      // Retreat Session Feedback
+      window.location.href = "";
     }
+  }
 
-    render () {
-        return (
-            <div>Please wait while we re-direct you</div>
-        )
-    }
+  render() {
+    return (
+      <div>Please wait while we re-direct you</div>
+    )
+  }
 }
 
 export default TzNavCoPFeedback;
