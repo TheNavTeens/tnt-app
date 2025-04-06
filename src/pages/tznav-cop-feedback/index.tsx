6f54844b1da7e99409136059004231ea597871de
julwrites <mail@tehj.io>
2025-04-06 14:59:27 +0800

    Updating links for Vision and Intention

--- src/pages/tznav-cop-feedback/index.tsx
+++ src/pages/tznav-cop-feedback/index.tsx
@@ -12,10 +12,10 @@ class TzNavCoPFeedback extends React.Component {
       window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfpWYV6Ar7RAG-r3ySULHdVtmbuYpp40TaqUgrKZUn_o0UEWg/viewform?usp=header";
     } else if (session == "vision") {
       // Retreat Session Feedback
-      window.location.href = "";
+      window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSduCN3Riyp8njuFcJmoThfonuq77Q8w4X49LTKnG4xZkUgBJA/viewform?usp=header";
     } else if (session == "intention") {
       // Retreat Session Feedback
-      window.location.href = "";
+      window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScQpWghtg9bwLds6-v6mpf9dwcrRAmQGESIe3bEu_o6MGatuw/viewform?usp=header";
     }
   }
 
