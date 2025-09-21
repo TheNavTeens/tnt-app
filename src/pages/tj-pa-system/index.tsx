7ad756153e94404c5d97cf6e8ff9e9041eb73c9f
julwrites <mail@tehj.io>
2025-09-21 16:27:32 +0800

    Adding React

--- src/pages/tj-pa-system/index.tsx
+++ src/pages/tj-pa-system/index.tsx
@@ -1,14 +1,17 @@
+import React from 'react';
 
-const TJPASystem = () => {
-  componentDidMount() {
-    window.location.href = "https://docs.google.com/document/d/e/2PACX-1vR31vwphgLurp3NuRxDggtqQpiR0gsJRb3FFOQKK_GD8vRje8NEGoFgaqNzp9ynTD2sFhu9qeC0mDQ2/pub";
-  }
+class TJPASystem extends React.Component {
 
-  render() {
-    return (
-      <div>Please wait while we re-direct you</div>
-    )
+  const TJPASystem = () => {
+    componentDidMount() {
+      window.location.href = "https://docs.google.com/document/d/e/2PACX-1vR31vwphgLurp3NuRxDggtqQpiR0gsJRb3FFOQKK_GD8vRje8NEGoFgaqNzp9ynTD2sFhu9qeC0mDQ2/pub";
+    }
+
+    render() {
+      return (
+        <div>Please wait while we re-direct you</div>
+      )
+    }
   }
-}
 
 export default TJPASystem
