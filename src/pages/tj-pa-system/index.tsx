4ba770296008327a2659245a2d65d00d4b1a6e30
julwrites <mail@tehj.io>
2025-09-21 16:14:19 +0800

    Removing VStack

--- src/pages/tj-pa-system/index.tsx
+++ src/pages/tj-pa-system/index.tsx
@@ -1,18 +1,10 @@
-import {
-  VStack,
-} from '@chakra-ui/react'
-
 import { Container } from '../../components/Container'
-import { Banner } from '../../components/Banner'
 
 const TJPASystem = () => {
   return (
     <div>
       <Container height="100vh">
-        <Banner />
-        <VStack overflow="scroll">
-          <iframe src="https://docs.google.com/document/d/e/2PACX-1vR31vwphgLurp3NuRxDggtqQpiR0gsJRb3FFOQKK_GD8vRje8NEGoFgaqNzp9ynTD2sFhu9qeC0mDQ2/pub?embedded=true"></iframe>
-        </VStack>
+        <iframe src="https://docs.google.com/document/d/e/2PACX-1vR31vwphgLurp3NuRxDggtqQpiR0gsJRb3FFOQKK_GD8vRje8NEGoFgaqNzp9ynTD2sFhu9qeC0mDQ2/pub?embedded=true"></iframe>
       </Container>
     </div>
   )
