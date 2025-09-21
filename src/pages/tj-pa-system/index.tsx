f57c4c3f1a6726700bf6a7d5c42a5f2e31f81a84
julwrites <mail@tehj.io>
2025-09-21 16:16:02 +0800

    Changing to a redirct for simplicity

--- src/pages/tj-pa-system/index.tsx
+++ src/pages/tj-pa-system/index.tsx
@@ -1,13 +1,14 @@
-import { Container } from '../../components/Container'
 
 const TJPASystem = () => {
-  return (
-    <div>
-      <Container height="100vh">
-        <iframe src="https://docs.google.com/document/d/e/2PACX-1vR31vwphgLurp3NuRxDggtqQpiR0gsJRb3FFOQKK_GD8vRje8NEGoFgaqNzp9ynTD2sFhu9qeC0mDQ2/pub?embedded=true"></iframe>
-      </Container>
-    </div>
-  )
+  componentDidMount() {
+    window.location.href = "https://docs.google.com/document/d/e/2PACX-1vR31vwphgLurp3NuRxDggtqQpiR0gsJRb3FFOQKK_GD8vRje8NEGoFgaqNzp9ynTD2sFhu9qeC0mDQ2/pub";
+  }
+
+  render() {
+    return (
+      <div>Please wait while we re-direct you</div>
+    )
+  }
 }
 
 export default TJPASystem
